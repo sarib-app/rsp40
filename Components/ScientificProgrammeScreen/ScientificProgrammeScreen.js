bc0b0dac9c9ef73f2a63ec880d4119cf0cf70acf
sarib-app <saribkhann84@gmail.com>
2024-09-05 02:02:21 +0500

    ds

--- Components/ScientificProgrammeScreen/ScientificProgrammeScreen.js
+++ Components/ScientificProgrammeScreen/ScientificProgrammeScreen.js
@@ -1,39 +1,39 @@
 import React, { useEffect } from 'react';
 import { View, Button, StyleSheet, Alert } from 'react-native';
 import MainHeader from '../Global/Header';
-import { Buffer } from 'buffer';
-import * as FileSystem from 'expo-file-system';
+// import { Buffer } from 'buffer';
+// import * as FileSystem from 'expo-file-system';
 import * as Sharing from 'expo-sharing';
 
 const ScientificProgrammeScreen = () => {
   
   const downloadAndSharePDF = async () => {
-    try {
-      const pdfUrl = 'https://rsp40.com/upload/1724242095.pdf'; // Replace with your PDF URL
+    // try {
+    //   const pdfUrl = 'https://rsp40.com/upload/1724242095.pdf'; // Replace with your PDF URL
       
-      // Fetch the PDF from the URL
-      const response = await fetch(pdfUrl);
-      const res = await response.arrayBuffer();
+    //   // Fetch the PDF from the URL
+    //   const response = await fetch(pdfUrl);
+    //   const res = await response.arrayBuffer();
       
-      // Convert the response to base64
-      const buff = Buffer.from(res);
-      const base64 = buff.toString('base64');
+    //   // Convert the response to base64
+    //   const buff = Buffer.from(res);
+    //   const base64 = buff.toString('base64');
 
-      // Define the local file URI
-      const fileUri = FileSystem.documentDirectory + `${encodeURI('ScientificProgramme')}.pdf`;
+    //   // Define the local file URI
+    //   const fileUri = FileSystem.documentDirectory + `${encodeURI('ScientificProgramme')}.pdf`;
 
-      // Save the PDF locally
-      await FileSystem.writeAsStringAsync(fileUri, base64, {
-        encoding: FileSystem.EncodingType.Base64,
-      });
+    //   // Save the PDF locally
+    //   await FileSystem.writeAsStringAsync(fileUri, base64, {
+    //     encoding: FileSystem.EncodingType.Base64,
+    //   });
 
-      // Share the PDF
-      await Sharing.shareAsync(fileUri);
+    //   // Share the PDF
+    //   await Sharing.shareAsync(fileUri);
       
-    } catch (error) {
-      console.error("Error downloading or sharing PDF:", error);
-      Alert.alert("Error", "There was an issue downloading or sharing the PDF. Please try again later.");
-    }
+    // } catch (error) {
+    //   console.error("Error downloading or sharing PDF:", error);
+    //   Alert.alert("Error", "There was an issue downloading or sharing the PDF. Please try again later.");
+    // }
   };
 
   return (
--- Components/SponsorshipTariffScreen/SponsorshipTariffScreen.js
+++ Components/SponsorshipTariffScreen/SponsorshipTariffScreen.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { View, Button, ActivityIndicator, StyleSheet } from 'react-native';
-import * as FileSystem from 'expo-file-system';
+// import * as FileSystem from 'expo-file-system';
 import * as Sharing from 'expo-sharing';
 import MainHeader from '../Global/Header'; // Adjust the import as needed
 
@@ -8,22 +8,22 @@ const SponsorshipTariffScreen = () => {
   const [isLoading, setIsLoading] = useState(false);
 
   const downloadAndShowPdf = async () => {
-    setIsLoading(true);
-    try {
-      const fileUri = `${FileSystem.documentDirectory}SponsorshipTariff.pdf`;
+    // setIsLoading(true);
+    // try {
+    //   const fileUri = `${FileSystem.documentDirectory}SponsorshipTariff.pdf`;
       
-      const downloadResumable = FileSystem.createDownloadResumable(
-        'https://rsp40.com/upload/1724312884.pdf', // Replace with your PDF URL
-        fileUri
-      );
+    //   const downloadResumable = FileSystem.createDownloadResumable(
+    //     'https://rsp40.com/upload/1724312884.pdf', // Replace with your PDF URL
+    //     fileUri
+    //   );
 
-      const { uri } = await downloadResumable.downloadAsync();
-      await Sharing.shareAsync(uri);
-    } catch (error) {
-      console.error('Error downloading or sharing the PDF:', error);
-    } finally {
-      setIsLoading(false);
-    }
+    //   const { uri } = await downloadResumable.downloadAsync();
+    //   await Sharing.shareAsync(uri);
+    // } catch (error) {
+    //   console.error('Error downloading or sharing the PDF:', error);
+    // } finally {
+    //   setIsLoading(false);
+    // }
   };
 
   return (
